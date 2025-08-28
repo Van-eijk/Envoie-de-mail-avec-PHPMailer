e720090d1d0503cf5cc7a1b088472ad8b12f9e1e
van <signingvan@yahoo.com>
2025-08-28 01:57:15 +0100

    Ajout des pièces jointes

--- index.php
+++ index.php
@@ -18,16 +18,23 @@ try {
 
     // Expéditeur et destinataire
     $mail->setFrom('vaneijkdjeatsa@gmail.com', 'INNOTECH STORE');
-    $mail->addAddress('van2021@outlook.fr', 'chef agence');
+    $mail->addAddress('van2021@outlook.fr', 'MAVIANCE');
 
     // Contenu du mail
     $mail->isHTML(true);
-    $mail->Subject = 'PROMOTION AU GRADE SUPERIEUR';
-    $mail->Body    = '<h2>Bonjour M.SIGNING</h2><p>Nous tenons à vous féliciter pour votre nouveau poste comme chef d.agence.</p>';
+    $mail->Subject = 'FACTURE';
+    $mail->Body    = '<h2>Bonjour Monsieur/Madame,</h2><p>Vous trouverez en pièce jointe, la facture et le bon de commande.</p>';
     //$mail->AltBody = 'Bonjour, bienvenue dans la plateforme de gestion des stagiaires.';
 
+    // Ajouter une pièce jointe (PDF, DOCX, etc.)
+    $mail->addAttachment('Doc/BC.pdf');  
+    $mail->addAttachment('Doc/Fac.pdf');  
+
+    // Tu peux ajouter plusieurs pièces jointes
+    // $mail->addAttachment('documents/autre_fichier.docx');
+
     $mail->send();
-    echo "Email envoyé avec succès !";
+    echo "Email avec pièce jointe envoyé avec succès !";
 } catch (Exception $e) {
     echo "Erreur lors de l’envoi : {$mail->ErrorInfo}";
 }
